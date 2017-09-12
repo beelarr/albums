f4a62396d1403e87dda39554151a595a841cd1d0
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-09-12 13:44:26 -0500

    Added more card styling with images of artist and album name

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,19 +3,59 @@ import Card from './Card'
 import CardSection from './CardSection'
 import {
     View,
+    Image,
     Text
 } from 'react-native';
 
-const AlbumDetail = (props) => { //props.data is my data from album list passing to card
+const AlbumDetail = ({ data }) => { //props.data is my data from album list passing to card
+    const { title, artist, thumbnail_image, image } = data;
     return (
 
         <Card>
             <CardSection>
-                <Text>{props.data.title}</Text>
+                <View style = {styles.thumbnailContainerStyle}>
+                    <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }}/>
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text style={styles.headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
+            <CardSection>
+                <Image style={styles.imageStyle} source = {{uri: image}}/>
+            </CardSection>
+
         </Card>
 
     )
 };
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    headerTextStyle: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+
+};
+
 
 export default AlbumDetail;
